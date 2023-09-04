b894ed27e62b646f3e6e9c8dcffabe304f4041e5
Simon Prophet <simon.s.prophet@outlook.com>
2023-09-04 20:14:05 +0100

    add advance CSS and JS code to buttom

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const toggle = document.querySelector('#toggle');
+const navbar = document.querySelector('#navbar');
+
+toggle.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
